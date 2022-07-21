05d4f134d9fee95c0f038b91e8c4738369979e3e
anthonyq98 <antoqdublin@gmail.com>
2022-07-21 23:00:50 +0100

    Using Terraform Cloud to handle terraform state files, editing .tf to configure this

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "anthony-test"
+
+        workspaces {
+          name = "getting-started"
+        }
+    }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.22.0"
+    }
+  }
+}
+
 locals {
     project_name = "Anthony"
 }
--- providers.tf
+++ providers.tf
@@ -1,20 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.22.0"
-    }
-  }
-}
-
 provider "aws" {
   region  = "us-east-1"
-  profile = "default"
 }
 
 
 provider "aws" {
   region  = "eu-west-1"
-  profile = "default"
   alias = "eu"
 }
